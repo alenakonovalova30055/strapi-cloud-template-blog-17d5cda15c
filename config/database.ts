73d9c6c153c4f6cae1570423370ea6d3343749b6
Alena <alena@Alenas-MacBook-Air.local>
2026-02-08 21:39:18 +0100

    fix: restore correct database.ts

--- config/database.ts
+++ config/database.ts
@@ -1,38 +1,42 @@
-ssl: {
-  rejectUnauthorized: false,
-},
 import path from 'path';
 
 export default ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
-
-    postgres: {
-  connection: {
-    host: env('DATABASE_HOST', 'localhost'),
-    port: env.int('DATABASE_PORT', 5432),
-    database: env('DATABASE_NAME', 'strapi'),
-    user: env('DATABASE_USERNAME', 'strapi'),
-    password: env('DATABASE_PASSWORD', 'strapi'),
-    ssl: {
-      rejectUnauthorized: false,
-    },
-    schema: env('DATABASE_SCHEMA', 'public'),
-  },
-  pool: {
-    min: env.int('DATABASE_POOL_MIN', 2),
-    max: env.int('DATABASE_POOL_MAX', 10),
+    mysql: {
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false) && {
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        },
       },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
-
+    postgres: {
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false) && {
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+        },
+        schema: env('DATABASE_SCHEMA', 'public'),
+      },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    },
     sqlite: {
       connection: {
         filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
       },
       useNullAsDefault: true,
     },
-
   };
 
   return {
@@ -43,4 +47,3 @@ export default ({ env }) => {
     },
   };
 };
-
