ccf8b5db294f01c4ab8bfd0f2853849bc38ee1e3
Alena <alena@Alenas-MacBook-Air.local>
2026-02-08 21:26:00 +0100

    fix: enable SSL for Strapi Cloud PostgreSQL

--- config/database.ts
+++ config/database.ts
@@ -1,3 +1,6 @@
+ssl: {
+  rejectUnauthorized: false,
+},
 import path from 'path';
 
 export default ({ env }) => {
@@ -6,20 +9,20 @@ export default ({ env }) => {
   const connections = {
 
     postgres: {
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: {
-        min: env.int('DATABASE_POOL_MIN', 2),
-        max: env.int('DATABASE_POOL_MAX', 10),
+  connection: {
+    host: env('DATABASE_HOST', 'localhost'),
+    port: env.int('DATABASE_PORT', 5432),
+    database: env('DATABASE_NAME', 'strapi'),
+    user: env('DATABASE_USERNAME', 'strapi'),
+    password: env('DATABASE_PASSWORD', 'strapi'),
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    schema: env('DATABASE_SCHEMA', 'public'),
+  },
+  pool: {
+    min: env.int('DATABASE_POOL_MIN', 2),
+    max: env.int('DATABASE_POOL_MAX', 10),
       },
     },
 
